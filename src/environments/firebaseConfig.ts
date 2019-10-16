325e793602df7771a0d3c9c5842227d7d653447e
Bala <Ankith.Bala@sony.com>
2019-10-16 23:26:37 +0530

    first commit

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,10 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyDSbXmhkzq7pTM4oZmTDWNVj8N1YnhHbkw",
+  authDomain: "mybiddingfire.firebaseapp.com",
+  databaseURL: "https://mybiddingfire.firebaseio.com",
+  projectId: "mybiddingfire",
+  storageBucket: "mybiddingfire.appspot.com",
+  messagingSenderId: "108179012391",
+  appId: "1:108179012391:web:42504d2435e97a77798702",
+  measurementId: "G-GM5HDVTEDR"
+};
